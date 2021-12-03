97ba8bd24b9a20c2eac45e8a68972f8180633965
arunkudiyal <arunkudiyal@icloud.com>
2021-12-03 18:16:30 +0530

    DOM - Day 2

--- DOM/scripts.js
+++ DOM/scripts.js
@@ -1,6 +1,6 @@
 // documnet --> Object representation of the Documnet (HTML)
 
-console.dir(document)
+// console.dir(document)
 
 // console.log(document.URL)
 // console.log(document.all);
@@ -56,3 +56,140 @@ console.dir(document)
 // console.log(headingTwo)
 
 // document.getElementsByTagName('h2')[1].innerText = 'My Items'
+
+// 4. 
+// querySelector -> id(#) + className(.) + tagName
+// It only returns the first instance of the chosed id, className or tagName
+// const headerTitle = document.querySelector('#header-title')
+// console.log(headerTitle)
+
+// headerTitle.textContent = 'Changed Content...'
+
+// const listItems = document.querySelector('.list-group-item')
+// console.log(listItems)
+
+// const headingTwo = document.querySelector('h2')
+// console.log(headingTwo)
+
+// 5.
+// querySelectorAll() -> id, className or tagName
+// const headerTitle = document.querySelectorAll('#header-title')
+// console.log(headerTitle)
+
+// const listItems = document.querySelectorAll('.list-group-item')
+// console.log(listItems)
+
+// for(let i=0; i < listItems.length; i++) {
+//     if(i%2 === 0) {
+//         listItems[i].style.backgroundColor = 'blue'
+//         listItems[i].style.color = 'white'
+//     }
+// }
+
+// const headingTwo = document.querySelectorAll('h2')
+// console.log(headingTwo)
+
+// headingTwo[0].style.color = 'grey'
+// headingTwo[1].style.color = 'grey'
+
+
+// DOM EVENTS, EVENT LISTNERS & EVENT PARAMETERS
+// Event -> Actions that you do in the app (click, double-click, scroll,...)
+// Event Listner -> Make sure what to do on what action - function()
+
+// 1. Access the element
+// const btn = document.querySelector('#button')
+// console.log(btn)
+
+// // 2. addEventListner() to the element
+// btn.addEventListener('click', () => {
+//     console.log('Button was Clicked...')
+// })
+
+// // const clickEventListner = () => {
+// //     console.log('Click Event Ocuuered')
+// // }
+
+// // const submitButton = document.querySelector('#submitBtn')
+// // submitButton.addEventListener('click', clickEventListner)
+
+// const submitButton = document.querySelector('#submitBtn')
+// console.log(submitButton)
+
+// // Event Parameter - A parameter passed to a function / method which listnes to an event
+// submitButton.addEventListener('click', (e) => {
+//     e.preventDefault()
+//     console.log(e)
+//     console.log('Form was submitted successfully...')
+// })
+
+
+// const textInput = document.querySelector('#text-input')
+// console.log(textInput)
+
+// Events - keydown, keyup
+// textInput.addEventListener('keyup', (e) => {
+//     // console.log(e)
+//     console.log(e.target.value)
+// })
+
+// cut Event
+// textInput.addEventListener('cut', () => {
+//     console.log('CUT Evenet Occured...')
+// })
+
+// textInput.addEventListener('copy', () => {
+//     console.log('COPY Evenet Ocuuered!')
+// })
+
+// textInput.addEventListener('paste', () => {
+//     console.log('PASTE Evenet Occured...')
+// })
+
+// const listItems = document.querySelectorAll('.list-group-item')
+// console.log(listItems)
+
+// for(let i=0 ; i < listItems.length; i++) {
+//     listItems[i].addEventListener('dblclick', () => {
+//         listItems[i].style.backgroundColor = 'blue'
+//         listItems[i].style.color = 'white'
+//     })
+// }
+
+// for(let i=0 ; i < listItems.length; i++) {
+//     listItems[i].addEventListener('click', () => {
+//         listItems[i].style.backgroundColor = 'grey'
+//         listItems[i].style.color = 'black'
+//     })
+// }
+
+
+// CREATING HTML ELEMENT USING DOM
+
+// const div = document.createElement('div')
+// console.log(div)
+
+// // set ID and class - setAttribute
+// div.setAttribute('id', 'my-div')
+// div.setAttribute('class', 'my-div-class')
+// console.log(div)
+
+// const submitBtn = document.querySelector('#submitBtn')
+// submitBtn.addEventListener('click', (e) => {
+//     e.preventDefault()
+
+//     const text = document.querySelector('#text-input').value
+
+//     const li = document.createElement('li')
+//     li.setAttribute('class', 'list-group-item')
+//     li.innerText = text
+
+//     console.log(li)
+
+//     // Step 1 - Find the parent element
+//     const ul = document.querySelector('#items')
+    
+//     // Step 2 - Put the element you created as a child to the parent
+//     // parentVariable.appendChild(newElement)
+//     ul.appendChild(li)
+// })
